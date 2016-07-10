8eaa82dd46a5e7343c48ddfb8e0ca50ab29b65ff
namth <namth@DESKTOP-QGPJMF8>
2016-07-10 19:51:19 +0700

    route implement for app

--- TitiShop.Web/app/app.js
+++ TitiShop.Web/app/app.js
@@ -1,1 +1,16 @@
-﻿
+﻿/// <reference path="D:\Resource Web\ASP MVC\TitiShop\git\TitiShop.Web\Assets/admin/libs/angular/angular.js" />
+
+(function () {
+    angular.module('titishop', ['titishop.products', 'titishop.common']).config(config);
+
+    config.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+    function config($stateProvider, $urlRouterProvider) {
+        $stateProvider.state('home', {
+            url: "/admin",
+            templateUrl: "/app/components/home/homeView.html",
+            controller: "homeController"
+        });
+        $urlRouterProvider.otherwise('/admin');
+    }
+})();
---
+++ TitiShop.Web/app/components/products/products.module.js
@@ -0,0 +1,20 @@
+﻿(function () {
+    angular.module('titishop.products', ['titishop.common']).config(config);
+
+    config.$inject = ['$stateProvider', '$urlRouterProvider'];
+    function config($stateProvider, $urlRouterProvider) {
+        $stateProvider.state('products', {
+            url: "/products",
+            templateUrl: "/app/components/products/productListView.html",
+            controller: "productListController"
+        }).state('product_add', {
+            url: "/product_add",
+            templateUrl: "/app/components/products/productAddView.html",
+            controller: "productAddController"
+        }).state('product_edit', {
+            url: "/product_edit",
+            templateUrl: "/app/components/products/productEditView.html",
+            controller: "productEditController"
+        });
+    }
+})();
