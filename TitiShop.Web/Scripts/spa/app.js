1c5eb8717ef5a534c3dcdcb614b4d84c11c40dec
namth <namth@DESKTOP-QGPJMF8>
2016-07-10 13:58:13 +0700

    what is $scope and $rootScope?

---
+++ TitiShop.Web/Scripts/spa/app.js
@@ -0,0 +1,21 @@
+﻿/// <reference path="../plugins/angular/angular.js" />
+
+var myApp = angular.module('myModule', []);
+
+myApp.controller("schoolController", schoolController);
+myApp.controller("studentController", studentController);
+myApp.controller("teacherController", teacherController);
+
+myApp.$inject = ['$scope'];
+
+function schoolController($scope) {
+    $scope.message = "Announcement from school";
+}
+
+function studentController($rootScope,$scope) {
+    $scope.message = "This is my message from student";
+}
+
+function teacherController($scope) {
+    $scope.message = "This is my message from teacher";
+}
