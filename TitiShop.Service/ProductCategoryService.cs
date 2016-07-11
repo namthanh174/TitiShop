1bdaf00d3f7db25d93a96af7ba3d36dc7016e517
namth <namth@DESKTOP-QGPJMF8>
2016-07-11 15:21:49 +0700

    searching for list

--- TitiShop.Service/ProductCategoryService.cs
+++ TitiShop.Service/ProductCategoryService.cs
@@ -18,6 +18,7 @@ namespace TitiShop.Service
         ProductCategory Delete(int id);
 
         IEnumerable<ProductCategory> GetAll();
+        IEnumerable<ProductCategory> GetAll(string keyword);
 
         IEnumerable<ProductCategory> GetAllByParentId(int parentId);
 
@@ -49,6 +50,18 @@ namespace TitiShop.Service
         public IEnumerable<ProductCategory> GetAll()
         {
             return _ProductCategoryRepository.GetAll();
+        }
+
+        public IEnumerable<ProductCategory> GetAll(string keyword)
+        {
+            if (!string.IsNullOrEmpty(keyword))
+            {
+                return _ProductCategoryRepository.GetMulti(x => x.Name.Contains(keyword) || x.Description.Contains(keyword) || x.Alias.Contains(keyword));
+            }else
+            {
+                return _ProductCategoryRepository.GetAll();
+            }
+            
         }
 
         public IEnumerable<ProductCategory> GetAllByParentId(int parentId)
